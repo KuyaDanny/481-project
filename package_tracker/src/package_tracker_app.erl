d39877e51d0096ece69723c3d0600a1469536323
KuyaDanny <97204225+KuyaDanny@users.noreply.github.com>
2023-06-16 13:44:34 -0600

    cowboy stuff

--- package_tracker/src/package_tracker_app.erl
+++ package_tracker/src/package_tracker_app.erl
@@ -10,6 +10,14 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+     Dispatch = cowboy_router:compile([
+        { <<"_">>, [{<<"/">>, hello_handler, []}]}
+    ]),
+     {ok, _} = cowboy:start_clear(
+        hello_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+     ),
     package_tracker_sup:start_link().
 
 stop(_State) ->
