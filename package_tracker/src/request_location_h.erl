e3692c4328d320fcd0400784eec72d40e07304d2
deoelvenmage <deoelvenmage@gmail.com>
2023-06-26 13:06:08 -0600

    made changes that surely will prep data for jsx

--- package_tracker/src/request_location_h.erl
+++ package_tracker/src/request_location_h.erl
@@ -9,7 +9,7 @@ init(Req0, Opts) ->
 	Package_id = decode_helper(jsx:decode(Data)),
 	Package_Data = request_location:req_loc(Package_id),
     io:format("~w~n", [Package_Data]),
-    Response_Data = jsx:encode(Package_Data),
+    Response_Data = jsx:encode(encode_helper(Package_Data)),
     io:format("~w~n", [Response_Data]),
 	Req = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/json">>
@@ -26,5 +26,25 @@ decode_helper(Data) ->
 
 encode_helper(Package_Data) ->
     {Lat, Lon, History} = Package_Data,
-    History.
+
+    Revised_lat = case is_integer(Lat) or is_float(Lat) of
+        true -> Lat;
+        _ -> <<Lat>>
+    end,
+    Revised_lon = case is_integer(Lon) or is_float(Lon) of
+        true -> Lon;
+        _ -> <<Lon>>
+    end,
+
+    % history needs mapped to be json objects, not tuple
+    History_convert_fun = fun({Location_id, Time, Status}) ->
+        #{<<"location_id">> => <<Location_id>>, <<"time">> => Time, <<"Status">> => <<Status>>}
+        end,
+    Revised_history = lists:map(History, History_convert_fun),
+    Response = #{<<"lat">> => Revised_lat,
+                <<"lon">> => Revised_lon,
+                "history" => Revised_history},
+
+    Response.
+
 
