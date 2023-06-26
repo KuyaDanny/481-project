a1c56b800995ed309a0be9ebf94a2ec131ec3021
KuyaDanny <97204225+KuyaDanny@users.noreply.github.com>
2023-06-26 10:39:06 -0600

    update

--- package_tracker/src/enter_center_h.erl
+++ package_tracker/src/enter_center_h.erl
@@ -6,6 +6,7 @@
 init(Req0, Opts) ->
 
 	{ok,Data,_} = cowboy_req:read_body(Req0),
+	io:format(Data),
 	[Package_id, Location_id, Time|_] = jsx:decode(Data),
 	enter_center:enter(Package_id, Location_id, Time),
 	Req = cowboy_req:reply(200, #{
