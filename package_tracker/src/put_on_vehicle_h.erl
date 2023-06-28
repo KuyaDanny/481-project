efd67458bbd974d02c8baeb1769447a6656d7f21
deoelvenmage <deoelvenmage@gmail.com>
2023-06-28 11:29:36 -0600

    added put_vehicle_h helper

--- package_tracker/src/put_on_vehicle_h.erl
+++ package_tracker/src/put_on_vehicle_h.erl
@@ -5,9 +5,19 @@
 init(Req0, Opts) ->
 
 	{ok,Data,_} = cowboy_req:read_body(Req0),
-	[Package_id, Vehicle_id, Time|_] = jsx:decode(Data),
+	{Package_id, Vehicle_id, Time} = helper(jsx:decode(Data)),
 	put_on_vehicle:put(Package_id, Vehicle_id, Time),
 	Req = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/json">>
 	}, "[\"done\"]", Req0),
 	{ok, Req, Opts}.
+
+helper(Data) ->
+    try
+        {_,Package_id} = maps:find(<<"Package_id">>, Data),
+        {_,Vehicle_id} = maps:find(<<"Vehicle_id">>, Data),
+        {_,Time} = maps:find(<<"Time">>, Data) 
+        of _ -> {Package_id, Vehicle_id, Time}
+    catch
+        _:_ -> {error, error, error}
+    end.
